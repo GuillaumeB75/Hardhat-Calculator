99ffad7da2cd094aa13154a8bc55f421c661f097
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-05-20 18:47:07 +0200

    script add & modification

---
+++ scripts/deploy-Calculator.js
@@ -0,0 +1,30 @@
+const hre = require("hardhat");
+
+const main = async () => {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Calculator = await hre.ethers.getContractFactory("Calculator");
+  const calculator = await calculator.deploy("Hello, Hardhat!");
+
+  await calculator.deployed();
+
+  // Affichage du déployement 
+  console.log("Calculator deployed to:", calculator.address);
+
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
