a3dee58dab7ef3d7865302238998350ced1367f2
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-05-20 18:28:52 +0200

    test file corrections

--- test/Calculator-test.js
+++ test/Calculator-test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 
 describe("Calculator", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Should return the right addition result", async function () {
     const Calculator = await ethers.getContractFactory("Calculator");
     const calculator = await Calculator.deploy();
 
@@ -9,4 +9,41 @@ describe("Calculator", function () {
     expect(await calculator.add(2, 3)).to.equal(5);
 
   });
+
+  it("Should return the right substraction result", async function () {
+    const Calculator = await ethers.getContractFactory("Calculator");
+    const calculator = await Calculator.deploy();
+
+    await calculator.deployed();
+    expect(await calculator.sub(4, 3)).to.equal(1);
+
+  });
+
+  it("Should return the right multiplication result", async function () {
+    const Calculator = await ethers.getContractFactory("Calculator");
+    const calculator = await Calculator.deploy();
+
+    await calculator.deployed();
+    expect(await calculator.mul(2, 3)).to.equal(6);
+
+  });
+
+  it("Should return the right division result", async function () {
+    const Calculator = await ethers.getContractFactory("Calculator");
+    const calculator = await Calculator.deploy();
+
+    await calculator.deployed();
+    expect(await calculator.div(8, 4)).to.equal(2);
+
+  });
+
+  it("Should return the right modulo result", async function () {
+    const Calculator = await ethers.getContractFactory("Calculator");
+    const calculator = await Calculator.deploy();
+
+    await calculator.deployed();
+    expect(await calculator.mod(10, 2)).to.equal(0);
+
+  });
+
 });
